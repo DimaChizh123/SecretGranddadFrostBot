ac6e68e881b7055938c14300c4b30eb23127e79c
Chizh <dmitkr3381@gmail.com>
2025-07-06 18:06:09 +0300

    Added actions 3

--- app/handlers/common.py
+++ app/handlers/common.py
@@ -28,7 +28,7 @@ async def start(message: Message, command: CommandObject, state: FSMContext):
         else:
             await message.answer("Ошибка! Не найдено")
     else:
-        await message.answer(f'злооооооо!\nДанный бот реализует игру "Тайный Санта"', reply_markup=KB.room_manager)
+        await message.answer(f'Злоооооо!\nДанный бот реализует игру "Тайный Санта"', reply_markup=KB.room_manager)
 
 @router.message(F.text == "Управление комнатами")
 async def manage_rooms(message: Message):
