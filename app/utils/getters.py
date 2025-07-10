71c41016021a93658542fc7905758bbd09e0c370
Chizh <dmitkr3381@gmail.com>
2025-07-10 22:54:28 +0300

    Удаление неактивных из базы

--- app/utils/getters.py
+++ app/utils/getters.py
@@ -1,9 +1,11 @@
 from aiogram import Bot
+from aiogram.enums import ChatAction
 from aiogram.exceptions import TelegramForbiddenError
 
 
 async def get_tg_username(bot: Bot, user_id: int) -> str | None:
     try:
+        await bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING)
         chat = await bot.get_chat(user_id)
         return f"@{chat.username}" if chat.username else ""
     except TelegramForbiddenError:
